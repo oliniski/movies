3b406635660949fe121e6505bfc5b162e03da7fc
Bruno Oliniski <brunooliniski@gmail.com>
2023-03-21 21:48:22 -0300

    Server: Find movies

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,3 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { prisma } from "../../prisma";
 
-export async function appRoutes(app: FastifyInstance) {}
+export async function appRoutes(app: FastifyInstance) {
+  app.get('/movies', async (request) => {
+    return await prisma.movie.findMany()
+  })
+}
