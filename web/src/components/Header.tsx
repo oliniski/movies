6528042dde1bc3d87e38fd068d665fb9d2eebe2d
Bruno Oliniski <brunooliniski@gmail.com>
2023-03-22 11:00:29 -0300

    Web: Axios

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import logoImage from '../assets/logo.svg'
 import Update from './Update'
 
-
 export function Header() {
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
--- web/src/components/Movies.tsx
+++ web/src/components/Movies.tsx
@@ -1,14 +1,33 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { Movie } from "./Movie";
+import { api } from "../lib/axios"
+
+type Movie = {
+  id: number
+  title: string
+  poster: string
+  description: string
+  director: string
+  producer: string
+}[]
 
 export function Movies() {
+  const [ movie, setMovie ] = useState<Movie>([])
+
+  useEffect(() => {
+    api.get('/movies').then(response => {
+      setMovie(response.data)
+    })
+  }, [])
+
   return (
     <div className="w-full flex">
-      <button title="Previous" className="rounded-full  px-5 py-0 items-center active:text-tomato11">
+      <button title="Previous" className="rounded-full px-5 py-0 items-center active:text-tomato11">
           <CaretLeft />
         </button>
         
-      <div className="grid grid-rows-1 grid-cols-10 gap-3">
+      <div className="grid grid-rows-1 grid-cols-10 gap-5">
         <Movie />
         <Movie />
         <Movie />
